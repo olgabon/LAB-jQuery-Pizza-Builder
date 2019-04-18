a662e50298ebd371d0d75c3940bf5c1396a50e86
olgabon <olga.bondarenko.shams@gmail.com>
2019-04-18 15:04:30 +0200

    done

---
+++ starter-code/pizza.js
@@ -0,0 +1,59 @@
+// Write your Pizza Builder JavaScript in this file.
+$(function(){
+
+//default 
+$("section.crust").toggleClass("crust-gluten-free");
+$("section.sauce").toggleClass("sauce-white");
+
+$(".btn-pepperonni").click(function(){ 
+    $(".pep").toggle();
+$(".btn-pepperonni").toggleClass("active");
+$("li:contains('pepperonni')").toggle();
+calcPrice()
+})
+
+$(".btn-mushrooms").click(function(){ 
+    $(".mushroom").toggle();
+$(".btn-mushrooms").toggleClass("active");
+$("li:contains('mushrooms')").toggle();
+    calcPrice()
+})
+
+$(".btn-green-peppers").click(function(){ 
+    $(".green-pepper").toggle();
+$(".btn-green-peppers").toggleClass("active");
+$("li:contains('green peppers')").toggle();
+calcPrice()
+})
+
+$(".btn-sauce").click(function(){
+    $("section.sauce").toggleClass("sauce-white");  
+    $(".btn-sauce").toggleClass("active");
+//$("li:contains('white sauce')").toggle();  
+//calcPrice()
+})
+
+$(".btn-crust").click(function(){ 
+    $("section.crust").toggleClass("crust-gluten-free");
+    $(".btn-crust").toggleClass("active");
+    //$("li:contains('gluten-free crust')").toggle(); 
+    //calcPrice();
+})
+
+
+function calcPrice(){
+    var totalPrice = 18;
+    if($(".btn-mushrooms").hasClass("active")) {
+        totalPrice += 1
+    };
+    if($(".btn-pepperonni").hasClass("active")) {
+        totalPrice += 1
+    };
+    if($(".btn-green-peppers").hasClass("active")) {
+        totalPrice += 1
+    };
+    $("#totalPrice").html(totalPrice)
+
+}
+
+})
